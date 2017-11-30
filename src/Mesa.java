f7d0192b7f5170b92e56e79ead998f5788768e4c
MEGAZORD <fmatos@ice.ufjf.br>
2017-11-30 11:06:14 -0200

    Serialização de Mesa

--- src/Mesa.java
+++ src/Mesa.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,28 +9,61 @@ public class Mesa {
     List<Pedido> pedidos;
     int id;
 
-    public Mesa(){
+    public Mesa() {
         this.id = ++Mesa.lastId;
         this.pedidos = new ArrayList<>();
     }
 
-    public boolean isAvailable(){
-        for(Pedido p : pedidos){
-            if(p.isOpen()){
+    public Mesa(int id) {
+        this.id = id;
+        this.pedidos = new ArrayList<>();
+        if (id > lastId) lastId = id;
+    }
+
+    public boolean isAvailable() {
+        for (Pedido p : pedidos) {
+            if (p.isOpen()) {
                 return false;
             }
         }
         return true;
     }
 
-    public void addPedido(Pedido p){
-        if(!this.pedidos.contains(p)) {
+    public void addPedido(Pedido p) {
+        if (!this.pedidos.contains(p)) {
             this.pedidos.add(p);
         }
+        Manager.save();
     }
 
+    public String serialize() {
+        ArrayList<String> result = new ArrayList<String>();
+        result.add(String.valueOf(this.id));
+        for (Pedido p : pedidos) {
+            result.add("begin PEDIDO");
+            result.add(p.serialize());
+            result.add("end PEDIDO");
+        }
+        return String.join("\n", result);
+    }
+
+    public static Mesa deserialize(List<String> data) {
+        Mesa mesa = new Mesa(Integer.valueOf(data.get(0)));
+        for (int i = 2; i < data.size(); i++) {
+            ArrayList<String> pData = new ArrayList<>();
+            while (!data.get(i).equals("end PEDIDO")) {
+                pData.add(data.get(i));
+                i++;
+            }
+            mesa.pedidos.add(Pedido.deserialize(pData));
+            i++;
+        }
+        return mesa;
+    }
+
+
     @Override
-    public String toString(){
+    public String toString() {
         return "Mesa " + this.id;
     }
 }
